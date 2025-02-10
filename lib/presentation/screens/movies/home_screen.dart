0474878378ffd55166906b9d5b9d867b4963395b
Alejandro Mira <alejandro.mira7@gmail.com>
2025-02-10 11:06:33 -0500

    update code

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cine_app/presentation/widgets/widgets.dart';
 import 'package:cine_app/presentation/providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -35,14 +36,22 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlideshow(movies: nowPlayingMovies)
+        // Expanded( // dado el padre 'column' expanda todo lo posible
+        //   child: ListView.builder(
+        //     itemCount: nowPlayingMovies.length,
+        //     itemBuilder: (context, index) {
+        //       final movie = nowPlayingMovies[index];
+        //       return ListTile(
+        //         title: Text(movie.title),
+        //       );
+        //     },
+        //   ),
+        // )
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/widgets.dart
@@ -0,0 +1,2 @@
+export 'package:cine_app/presentation/widgets/shared/custom_appbar.dart';
+export 'package:cine_app/presentation/widgets/movies/movies_slideshow.dart';
