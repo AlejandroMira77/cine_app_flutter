575d82ff6b33fd771a6ffe0d10f297b47d3f9598
Alejandro Mira <alejandro.mira7@gmail.com>
2025-02-13 07:55:52 -0500

    config routes

--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:cine_app/config/helpers/human_formats.dart';
 import 'package:cine_app/domain/entities/movie.dart';
@@ -141,7 +142,10 @@ class _Slide extends StatelessWidget {
                         ),
                       );
                     }
-                    return FadeIn(child: child);
+                    return GestureDetector(
+                      onTap: () => context.push('/movie/${ movie.id }'),
+                      child: FadeIn(child: child)
+                    );
                   },
                 ),
               ),
